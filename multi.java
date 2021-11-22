640875523f99bf907acd8e05e0c5c09c27558631
Yogesh Kumar <yogesh7kumar6865@gmail.com>
2021-11-22 23:23:13 +0530

    The File is add

---
+++ multi.java
@@ -0,0 +1,9 @@
+public class multi {
+    public static void main(String[] args) {
+        int a  =  10;
+        int b =  20;
+        int multi = a*b;
+        System.out.print("The Multiplay Is :" +multi);
+        
+    }
+}
