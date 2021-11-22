aa919385c1588aa53a14510d5cc3c9f14aab1938
Yogesh Kumar <yogesh7kumar6865@gmail.com>
2021-11-22 22:46:02 +0530

    intial commit

---
+++ sum.java
@@ -0,0 +1,10 @@
+public class sum{
+    public static void main(String[] args){
+        int a = 5;
+        int b = 10;
+        int sum = a+b;
+        int sub = a-b;
+        System.out.println("The Sum is :" +sum + " And Sub Is :" +sub);
+
+    }
+}
